600a7c284528700a224cc485a1ab4166dc24be7f
Earnest Cain <caintre03@gmail.com>
2018-08-05 13:11:46 -0700

    added multiplyarray logic

--- app.js
+++ app.js
@@ -91,11 +91,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  return[multiply((multiply(multArr[0], multArr[1])[0]), multArr[2])[0], 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + multiply((multiply(multArr[0], multArr[1])[0]), multArr[2])[0] + '.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
