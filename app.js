265536a395451163b6701ea0995a8223058a7f88
Earnest Cain <caintre03@gmail.com>
2018-08-05 12:47:30 -0700

    finished sumandmultiply function

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -48,11 +48,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  return [sum((sum(a, b)[0]), c)[0], multiply((multiply(a, b)[0]), c)[0], a + ' and ' + b + ' and ' + c + ' sum to ' + (a+ b + c) + '.',
+  'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + (a * b * c) + '.'];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
